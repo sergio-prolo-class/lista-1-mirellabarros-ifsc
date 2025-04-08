ff5c635e850951487dcd3acf62b9f82ea7b32236
Mirella Barros <mirella.b@aluno.ifsc.edu.br>
2025-04-07 21:40:22 -0300

    Remoção de espaços na montagem do tabuleiro

--- exercicio-3/app/src/main/java/org/example/App.java
+++ exercicio-3/app/src/main/java/org/example/App.java
@@ -104,7 +104,7 @@ public class App {
 
         for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro[i].length; j++) {
-                System.out.print(tabuleiro[i][j] + " ");
+                System.out.print(tabuleiro[i][j]);
             }
             System.out.println();
         }
